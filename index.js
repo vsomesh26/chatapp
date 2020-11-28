c1425703b6d9e2df9ea9db22e2839fd0a2474e05
vsomesh26 <vsomesh26@gmail.com>
2020-11-28 09:35:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express()
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/assets'));
+
+io.on('connection', (socket) => {
+  
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  io.emit('chat message', ' Welcome to serverless deployment world');
+ 
+});
+ 
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+  //module.exports.hello = serverless(app);
